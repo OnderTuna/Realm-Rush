c027cd7a339a3238d098537748b0d54cbed246f8
OnderTuna <ondertuna1905@gmail.com>
2024-07-29 23:23:02 +0300

    enemy movement version 0.101

--- Realm Rush/Assets/Scripts/EnemyMover.cs
+++ Realm Rush/Assets/Scripts/EnemyMover.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path;
+    IEnumerator followNumerator;
+    private float delayTime = 1f;
 
     void Start()
     {
-        for (int i = 0; i < path.Count; i++)
-        {
-            Debug.Log($"Anlik olarak koordinatim: {path[i].transform.name}");
-        }
+        followNumerator = FollowPath();
+        StartCoroutine(followNumerator);
     }
     
     void Update()
     {
         
+    }
+
+    private IEnumerator FollowPath()
+    {
+        for (int i = 0; i < path.Count; i++)
+        {
+            transform.position = path[i].transform.position;
+            yield return new WaitForSeconds(delayTime);
+        }
     }
 }
