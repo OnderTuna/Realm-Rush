db4db2dde1f6396e9f9a2d0556040ad4a1cb568d
OnderTuna <ondertuna1905@gmail.com>
2024-08-05 20:32:15 +0300

    Fire System Version 0.101
    Düşman objelerden yakında olanı belirlenip o objeye saldırıldı.
    Belirli bir range eklendi.

--- Realm Rush/Assets/Scripts/TargetLocater.cs
+++ Realm Rush/Assets/Scripts/TargetLocater.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,60 @@ using UnityEngine;
 public class TargetLocater : MonoBehaviour
 {
     [SerializeField] private GameObject weapon;
+    [SerializeField] private float maxRange = 15f;
+    [SerializeField] private ParticleSystem fireEffect;
     private Transform target;
 
 
     void Start()
     {
-        target = FindObjectOfType<EnemyMover>().transform;
+
     }
-    
+
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies;
+        enemies = FindObjectsOfType<Enemy>();
+        Transform closestEnemy = null;
+        float maxDistance = Mathf.Infinity;
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemies[i].transform.position);
+            if (targetDistance < maxDistance)
+            {
+                closestEnemy = enemies[i].transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestEnemy;
+    }
 
     private void AimWeapon()
     {
+        float rangeCheck = Vector3.Distance(transform.position, target.transform.position);
         weapon.transform.LookAt(target);
+
+        if (rangeCheck < maxRange)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    private void Attack(bool value)
+    {
+        ParticleSystem.EmissionModule emissionModule = fireEffect.emission;
+        emissionModule.enabled = value;
     }
 }
